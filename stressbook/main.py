404caa8384f3ee514106996464cee293677b3715
Guan Hong <147469495+Prowind66@users.noreply.github.com>
2024-11-10 23:28:31 +0800

    added more files
    set up locust folder and the corresponding files.
    Can remove the file at the later stage if feels that the the things we are doing not right
    added partial home page , and partial login page

--- stressbook/main.py
+++ stressbook/main.py
@@ -1,11 +1,22 @@
 from flask import *
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
+@app.route("/register")
+def register_account():
+    return render_template("register.html")
+
+@app.route("/login")
+def login_account():
+    return render_template("login.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, thread=True)
