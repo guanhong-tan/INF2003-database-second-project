1214c6ed4549340ecef93922e45d75c03907b494
Guan Hong <147469495+Prowind66@users.noreply.github.com>
2024-11-11 02:21:59 +0800

    updated routing for the pages
    playing concert seating arrangement with svg , hopefully UI done by next two days

--- stressbook/main.py
+++ stressbook/main.py
@@ -18,5 +18,17 @@ def register_account():
 def login_account():
     return render_template("login.html")
 
+@app.route("/concerts")
+def concerts():
+    return render_template("concerts.html")
+
+@app.route("/threaters")
+def threaters():
+    return render_template("threaters.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
 if __name__ == "__main__":
     app.run(debug=True, thread=True)
